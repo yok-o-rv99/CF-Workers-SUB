1d0c22f9e2e111e165f5bb8e068dcf4bb2f00996
yok-o-rv99 <regview99@outlook.com>
2024-09-23 18:52:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -20,7 +20,7 @@ https://sub.xf.free.hr/auto
 
 let urls = [];
 //let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconverter = "sub-conver-bulianglin.yiball.monster";
+let subconverter = "conver-bulianglin.xxx";
 
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
